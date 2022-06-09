9c9a16937a0b124ba37ee4751f5262395d1fb945
João Victor Nicacio <74374178+JoaoVNicacio@users.noreply.github.com>
2022-06-09 20:43:30 -0300

    Add files via upload

---
+++ DesafioPalindromo.js
@@ -0,0 +1,17 @@
+function palindromeChecker(word){
+    // criando os arrays para a analise:
+    let palindrome= word.split("").reverse().join("")
+
+    // retorno da função:
+    return (palindrome === word) ? `${word} é um palíndromo` : `${word} não é um palíndromo`
+}
+
+// declaração e obtenção de valores:
+var wordInput = prompt("Digite uma palavra e iremos checar se é um palíndromo.")
+
+// chamando e exibindo a função:
+console.log(palindromeChecker(wordInput))
+// chamando com alert para exibir de forma visível ao usuário que não estiver no console:
+alert(palindromeChecker(wordInput))
+
+
